532defa3ee278cd9b1d16d304352ba35b91f7655
oza6ut0ne <33759728+oza6ut0ne@users.noreply.github.com>
2018-07-02 01:24:53 +0900

    add edge detection filters

---
+++ filter/can.py
@@ -0,0 +1,21 @@
+import cv2
+
+class Filter(object):
+    '''apply Canny edge detection'''
+
+    def __init__(self, params):
+        try:
+            self.thresh1 = float(params[0])
+        except (KeyError, ValueError, IndexError):
+            self.thresh1 = 50
+
+        try:
+            self.thresh2 = float(params[1])
+        except (KeyError, ValueError, IndexError):
+            self.thresh2 = 110
+
+    def apply(self, img, params):
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        canny = cv2.Canny(gray, self.thresh1, self.thresh2)
+        inverse = cv2.bitwise_not(canny)
+        return inverse
---
+++ filter/dil.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+
+class Filter(object):
+    '''apply dilate diff edge detection'''
+
+    def __init__(self, params):
+        self.nb8 = np.array([[1,1,1],[1,1,1],[1,1,1]],np.uint8)
+
+    def apply(self, img, params):
+        dilate = cv2.dilate(img, self.nb8, iterations=2)
+        diff = cv2.absdiff(img, dilate)
+        res = cv2.bitwise_not(diff)
+        return res
