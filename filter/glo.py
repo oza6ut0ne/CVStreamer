137b787e0cd5fab3acf0d4f10333e73c6a0eb544
oza6ut0ne <33759728+oza6ut0ne@users.noreply.github.com>
2018-07-03 22:42:59 +0900

    make glow filter faster

--- filter/glo.py
+++ filter/glo.py
@@ -19,22 +19,24 @@ class Filter(object):
         except (KeyError, ValueError, IndexError):
             self.rad = 4.7
 
-    def shadow(self, img, threshold):
-        copy = img.astype(np.float)
-        copy[copy < threshold] = 0
-        copy[copy != 0] -= threshold
-        copy *= 255.
-        copy /= (255. - threshold)
-        return copy.astype(np.uint8)
+        self.lut = np.arange(0, 256).reshape(256, 1)
+        self.lut[self.lut < self.thresh] = 0
+        self.lut[self.lut != 0] -= self.thresh
+        self.lut *= 255
+        self.lut //= (255 - self.thresh)
+        self.lut = self.lut.astype(np.uint8)
+
+    def shadow(self, img):
+        return  cv2.LUT(img, self.lut)                          
 
     def screen(self, img1, img2):
         inv1 = cv2.bitwise_not(img1)
         inv2 = cv2.bitwise_not(img2)
-        mul = 255. * (inv1 / 255.) * (inv2 / 255.)
+        mul = inv1 / 255 * inv2
         mul = mul.astype(np.uint8)
         return cv2.bitwise_not(mul)
 
     def apply(self, img, params):
-        copy = self.shadow(img, self.thresh)
+        copy = self.shadow(img)
         copy = cv2.GaussianBlur(copy, (0, 0), self.rad)
         return self.screen(copy, img)
