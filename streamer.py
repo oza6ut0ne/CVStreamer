922aad9df88ddf436620c691991fd67f8304154f
oza6ut0ne <33759728+oza6ut0ne@users.noreply.github.com>
2018-07-03 22:42:59 +0900

    make query params in collect order

--- streamer.py
+++ streamer.py
@@ -19,13 +19,25 @@ class CamHandler(AuthHandler):
     def get_html(self, img_src='cam.mjpg'):
         return '<img src="{}"/>'.format(img_src)
 
+    def parse_qs(self, qs, keep_blank_values=False, strict_parsing=False,
+                 encoding='utf-8', errors='replace'):
+        parsed_result = collections.OrderedDict()
+        pairs = urllib.parse.parse_qsl(qs, keep_blank_values, strict_parsing,
+                                       encoding=encoding, errors=errors)
+        for name, value in pairs:
+            if name in parsed_result:
+                parsed_result[name].append(value)
+            else:
+                parsed_result[name] = [value]
+        return parsed_result
+
     def do_GET(self):
         if not super().do_GET():
             return
 
         parsed_url = urllib.parse.urlparse(self.path)
         dirpath, filename = os.path.split(parsed_url.path)
-        query = urllib.parse.parse_qs(parsed_url.query, keep_blank_values=True)
+        query = self.parse_qs(parsed_url.query, keep_blank_values=True)
         root, ext = os.path.splitext(filename)
         if filename.endswith('.mjpg'):
             self.send_response(http.client.OK)
